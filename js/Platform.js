efb350be942bd5cc7b6f464a06c9d113f0403d53
Default8914 <114364229+Default8914@users.noreply.github.com>
2025-12-16 14:48:21 +0300

    Update Platform.js

--- js/Platform.js
+++ js/Platform.js
@@ -1,29 +1,13 @@
-> Иван:
 export class Platform {
   constructor({ x, y, w, h }) {
-    this.x = x; this.y = y; this.w = w; this.h = h;
+    this.x = x;
+    this.y = y;
+    this.w = w;
+    this.h = h;
   }
 
   draw(ctx) {
-    ctx.fillStyle = "rgba(255,255,255,0.12)";
+    ctx.fillStyle = "rgba(255,255,255,0.15)";
     ctx.fillRect(this.x, this.y, this.w, this.h);
-
-    ctx.fillStyle = "rgba(255,255,255,0.06)";
-    ctx.fillRect(this.x, this.y, this.w, 3);
-  }
-}
-
-> Иван:
-export class Platform {
-  constructor({ x, y, w, h }) {
-    this.x = x; this.y = y; this.w = w; this.h = h;
-  }
-
-  draw(ctx) {
-    ctx.fillStyle = "rgba(255,255,255,0.12)";
-    ctx.fillRect(this.x, this.y, this.w, this.h);
-
-    ctx.fillStyle = "rgba(255,255,255,0.06)";
-    ctx.fillRect(this.x, this.y, this.w, 3);
   }
 }
